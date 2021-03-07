5cc7484a2aab14871c5a046acedec4cf91f4f934
Marlaqk <ralph4scheu@gmail.com>
2021-03-07 20:59:30 +0100

    add angular/fire

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AngularFireModule } from '@angular/fire';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+const config = {
+  apiKey: "AIzaSyBKMqGKgF7O4btPubf5q1gDzWML1FnME6Q",
+  authDomain: "stuwork-trial.firebaseapp.com",
+  projectId: "stuwork-trial",
+  storageBucket: "stuwork-trial.appspot.com",
+  messagingSenderId: "921404920677",
+  appId: "1:921404920677:web:84cdce278c3377acb0078c"
+};
 
 @NgModule({
   declarations: [
@@ -10,7 +20,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(config)
   ],
   providers: [],
   bootstrap: [AppComponent]
